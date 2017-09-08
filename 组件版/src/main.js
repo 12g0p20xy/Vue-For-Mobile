7c8030ed4a9440897d8fda8d51de005717f07d6f
12g0p20xy <12g0p20xy@gmail.com>
2017-09-08 16:36:07 +0800

    add Vuex

--- 组件版/src/main.js
+++ 组件版/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './App.vue'
 import Routes from './routers'
+import store from './store'
 
 Vue.use(VueRouter)
 
@@ -12,5 +13,6 @@ const router = new VueRouter({
 new Vue({
   el: '#app',
   render: h => h(App),
-  router
+  router,
+  store
 })
--- 组件版/src/pages/rank.vue
+++ 组件版/src/pages/rank.vue
@@ -1,5 +1,17 @@
 <template>
   <div id="rank">
     <h3>排名</h3>
+    {{ msg }}
   </div>
 </template>
+
+<script>
+  
+  export default {
+    computed: {
+      msg() {
+        return this.$store.state.msg
+      }
+    }
+  }
+</script>
